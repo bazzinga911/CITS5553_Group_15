ad17b1653dda0bb1bf5f749a311b093a1ac5919f
bazzinga911 <54790688+bazzinga911@users.noreply.github.com>
2025-08-25 20:41:23 +0800

    Create test_comparisons.py

---
+++ backend/tests/test_comparisons.py
@@ -0,0 +1,8 @@
+"""
+Tests for comparison methods.
+"""
+
+def test_compare_max_per_cell():
+    # TODO: build small toy GeoDataFrames with known max values
+    # TODO: run compare() and assert delta is correct
+    pass
