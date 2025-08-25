4073a106e65a1a9a2775a9fa7de1adeaab395294
bazzinga911 <54790688+bazzinga911@users.noreply.github.com>
2025-08-25 20:41:05 +0800

    Create test_grid.py

---
+++ backend/tests/test_grid.py
@@ -0,0 +1,11 @@
+"""
+Tests for grid utilities.
+"""
+
+def test_make_regular_grid():
+    # TODO: build dummy bounds, make grid, assert expected number of cells
+    pass
+
+def test_assign_grid_id():
+    # TODO: test a few known points near edges
+    pass
