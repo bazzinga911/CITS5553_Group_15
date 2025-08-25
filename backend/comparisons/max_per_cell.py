3a396a216b11b684dc6b99eed4ad8ebb720cb048
bazzinga911 <54790688+bazzinga911@users.noreply.github.com>
2025-08-25 20:40:36 +0800

    Create max_per_cell.py

---
+++ backend/comparisons/max_per_cell.py
@@ -0,0 +1,8 @@
+"""
+Comparison method: MAX per grid cell.
+"""
+
+def compare(orig_pts_gdf, dl_pts_gdf, grid_gdf):
+    # TODO: compute max Te_ppm per cell for orig and dl
+    # TODO: attach to grid and compute delta
+    pass
