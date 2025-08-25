df808aa6e1f20031c58b6c8c076d4936469bc2b7
bazzinga911 <54790688+bazzinga911@users.noreply.github.com>
2025-08-25 20:38:08 +0800

    Create io_s3.py

---
+++ backend/pipeline/io_s3.py
@@ -0,0 +1,11 @@
+"""
+S3 I/O helpers (stub).
+"""
+
+def read_points(path):
+    # TODO: read Parquet / GeoParquet from S3/local into GeoDataFrame
+    pass
+
+def write_grid(path, gdf):
+    # TODO: write GeoDataFrame to S3/local
+    pass
